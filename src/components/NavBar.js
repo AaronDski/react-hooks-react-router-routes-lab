5ed880da5d87b6d8b616bd3381905a361c3c6833
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-30 17:02:15 -0600

    commit tree 6e266bc3fe1fad6b8070fd850d76ebff14a3dfca

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div className='navbar' style={{justifyContent: "spaceAround"}}>
+    <NavLink to='/' style={{justifyContent: "spaceAround"}} >Home</NavLink>
+
+    <NavLink to='/movies' style={{justifyContent: "spaceAround"}}>Movies</NavLink>
+    <NavLink to='/directors' style={{justifyContent: "spaceAround"}}>Directors</NavLink>
+    <NavLink to='/actors' style={{justifyContent: "spaceAround"}}>Actors</NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
